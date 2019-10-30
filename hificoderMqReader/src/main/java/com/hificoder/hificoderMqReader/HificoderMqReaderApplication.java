381c4b6d7e759499fd81622e12deae1decac680f
Madhusudana <newcompanymadhu@gmail.com>
2019-10-30 22:02:40 +0530

    Initial Commit

---
+++ hificoderMqReader/src/main/java/com/hificoder/hificoderMqReader/HificoderMqReaderApplication.java
@@ -0,0 +1,13 @@
+package com.hificoder.hificoderMqReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HificoderMqReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HificoderMqReaderApplication.class, args);
+	}
+
+}
---
+++ hificoderMqReader/src/test/java/com/hificoder/hificoderMqReader/HificoderMqReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hificoder.hificoderMqReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HificoderMqReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
