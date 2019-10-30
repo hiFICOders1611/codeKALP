b4564a0628e9f4d72b33ab594841d044e15d90cb
Gopakumar7 <57186010+Gopakumar7@users.noreply.github.com>
2019-10-30 22:05:44 +0530

    Add files via upload

---
+++ hificoderWeb/src/main/java/com/hificoder/hificoderWeb/HificoderWebApplication.java
@@ -0,0 +1,13 @@
+package com.hificoder.hificoderWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HificoderWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HificoderWebApplication.class, args);
+	}
+
+}
---
+++ hificoderWeb/src/test/java/com/hificoder/hificoderWeb/HificoderWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hificoder.hificoderWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HificoderWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
