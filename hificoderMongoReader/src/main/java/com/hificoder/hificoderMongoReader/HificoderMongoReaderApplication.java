5b8461fddf5b94227dbbf1d8b818a4152dd8a565
dinshetty14 <din.shetty@gmail.com>
2019-10-31 12:44:07 +0530

    Initial commit

---
+++ hificoderMongoReader/src/main/java/com/hificoder/hificoderMongoReader/HificoderMongoReaderApplication.java
@@ -0,0 +1,13 @@
+package com.hificoder.hificoderMongoReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HificoderMongoReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HificoderMongoReaderApplication.class, args);
+	}
+
+}
---
+++ hificoderMongoReader/src/test/java/com/hificoder/hificoderMongoReader/HificoderMongoReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hificoder.hificoderMongoReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HificoderMongoReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
