c9ca06bf2aa27c5cc65d85436a5ea0f57e753ff1
DinakaraShetty <din.shetty@gmail.com>
2019-11-10 23:17:53 +0530

    Spring-Data-Mongo CRUD module

---
+++ hificoderMongoReader/src/main/java/com/hificoder/hificoderMongo/controller/UserController.java
@@ -0,0 +1,100 @@
+package com.hificoder.hificoderMongo.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hificoder.hificoderMongo.dal.UserDAL;
+import com.hificoder.hificoderMongo.dal.UserRepository;
+import com.hificoder.hificoderMongo.model.User;
+
+@RestController
+@RequestMapping(value = "/")
+public class UserController {
+
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+	private final UserRepository userRepository;
+
+	private final UserDAL userDAL;
+
+	public UserController(UserRepository userRepository, UserDAL userDAL) {
+		this.userRepository = userRepository;
+		this.userDAL = userDAL;
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public User addNewUsers(@RequestBody User user) {
+		LOG.info("Saving user.");
+		return userRepository.save(user);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<User> getAllUsers() {
+		LOG.info("Getting all users.");
+		return userRepository.findAll();
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	public User getUser(@PathVariable String userId) {
+		LOG.info("Getting user with ID: {}.", userId);
+		return userRepository.findOne(userId);
+	}
+
+	// @RequestMapping(value = "/settings/{userId}", method = RequestMethod.GET)
+	// public Object getAllUserSettings(@PathVariable String userId) {
+	// User user = userRepository.findOne(userId);
+	// if (user != null) {
+	// return user.getUserSettings();
+	// } else {
+	// return "User not found.";
+	// }
+	// }
+
+	@RequestMapping(value = "/settings/{userId}", method = RequestMethod.GET)
+	public Object getAllUserSettings(@PathVariable String userId) {
+		User user = userRepository.findOne(userId);
+		if (user != null) {
+			return userDAL.getAllUserSettings(userId);
+		} else {
+			return "User not found.";
+		}
+	}
+
+	// @RequestMapping(value = "/settings/{userId}/{key}", method =
+	// RequestMethod.GET)
+	// public String getUserSetting(@PathVariable String userId, @PathVariable
+	// String key) {
+	// //User user = userRepository.findOne(userId);
+	// String setting = userDAL.getUserSetting(userId, key);
+	// LOG.info("Setting = "+setting);
+	// if (setting != null) {
+	// return setting;
+	// } else {
+	// return "Setting not found.";
+	// }
+	// }
+
+	@RequestMapping(value = "/settings/{userId}/{key}", method = RequestMethod.GET)
+	public String getUserSetting(@PathVariable String userId, @PathVariable String key) {
+		return userDAL.getUserSetting(userId, key);
+	}
+
+	@RequestMapping(value = "/settings/{userId}/{key}/{value}", method = RequestMethod.GET)
+	public String addUserSetting(@PathVariable String userId, @PathVariable String key, @PathVariable String value) {
+		User user = userRepository.findOne(userId);
+		if (user != null) {
+			user.getUserSettings().put(key, value);
+			userRepository.save(user);
+			return "Key added";
+		} else {
+			return "User not found.";
+		}
+	}
+}
