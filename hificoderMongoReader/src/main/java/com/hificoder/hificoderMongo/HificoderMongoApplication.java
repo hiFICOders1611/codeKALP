9d9bca06bea6f4ea509c26159cb2055cce914465
DinakaraShetty <din.shetty@gmail.com>
2019-11-10 23:14:53 +0530

    Spring-Data-Mongo CRUD module

--- hificoderMongoReader/src/main/java/com/hificoder/hificoderMongo/HificoderMongoApplication.java
+++ hificoderMongoReader/src/main/java/com/hificoder/hificoderMongo/HificoderMongoApplication.java
@@ -1,13 +1,13 @@
-package com.hificoder.hificoderMongoReader;
+package com.hificoder.hificoderMongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HificoderMongoReaderApplication {
+public class HificoderMongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HificoderMongoReaderApplication.class, args);
+		SpringApplication.run(HificoderMongoApplication.class, args);
 	}
 
 }
