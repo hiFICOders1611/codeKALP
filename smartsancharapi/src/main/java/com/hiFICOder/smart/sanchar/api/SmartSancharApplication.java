d4526c1fd33e9b8e7aab4e804182054e345d8189
Madhusudana <newcompanymadhu@gmail.com>
2019-11-16 12:13:27 +0530

    Add files via upload

---
+++ smartsancharapi/src/main/java/com/hiFICOder/smart/sanchar/api/SmartSancharApplication.java
@@ -0,0 +1,13 @@
+package com.hiFICOder.smart.sanchar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSancharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSancharApplication.class, args);
+	}
+
+}
