64dfa16b0ed4f753f717147f6c8449e22ceff14c
Madhusudana <newcompanymadhu@gmail.com>
2019-11-16 12:43:08 +0530

    Add files via upload

---
+++ smartsancharconsumer/src/main/java/com/hiFICOder/smart/sanchar/consumer/SmartsancharconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.hiFICOder.smart.sanchar.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsancharconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsancharconsumerApplication.class, args);
+	}
+
+}
---
+++ smartsancharconsumer/src/test/java/com/hiFICOder/smart/sanchar/consumer/SmartsancharconsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hiFICOder.smart.sanchar.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartsancharconsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
